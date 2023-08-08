af89096be22182fad5bb9d0e799e67b1c7e9a73e
ineusintia <ineusn73@gmail.com>
2023-08-08 17:51:19 +0700

    SIP - 02 expencesCategory

--- resources/views/contents/expensescategory/edit.blade.php
+++ resources/views/contents/expensescategory/edit.blade.php
@@ -7,7 +7,25 @@
           <div class="section-header">
             <h1>{{ $title }}</h1>
           </div>
-          
+          <div class="card">
+            <div class="card-header">
+                <div class="card-title my-auto">
+                    <h5 class="text-primary">Form Edit Kategori Pengeluaran</h5>
+                </div>
+            </div>
+            <div class="card-body">
+              <form action="/expenses-category/{id}/edit">
+                <div class="form-group mb-3">
+                  <label for="name" class="text-primary">Nama Kategori Pengeluaran :</label>
+                  <input type="text" class="form-control"value="pembelian barang">
+                </div>
+                <div class="float-right mt-5">
+                  <a href="/expenses-category" class="btn btn-danger">Cancel</a>
+                  <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+              </form>
+            </div>
+        </div>
         </section>
       </div>
 @endsection
--- resources/views/contents/expensescategory/index.blade.php
+++ resources/views/contents/expensescategory/index.blade.php
@@ -1,5 +1,13 @@
 @extends('app.main')
 
+@section('style')
+  @include('components.style.datatable')
+@endsection
+
+@section('script')
+    @include('components.script.datatable')
+@endsection
+
 @section('content')
      <!-- Main Content -->
      <div class="main-content">
@@ -7,6 +15,14 @@
           <div class="section-header">
             <h1>{{ $title }}</h1>
           </div>
+          @if (session('Berhasil'))
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                {{ session('Berhasil') }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            @endif
           <div class="card mb-4 dt-container">
             <div class="col-lg-12 mt-3">
                 <div class="btn-group dropright">
@@ -20,126 +36,33 @@
                         <button class="btn btn-sm btn-secondary col w-75 mb-2"><i class="fas fa-print dt-print"></i> Print</button>
                     </div>
                 </div>
-                <a href="/books/create" class="btn btn-sm btn-outline-primary rounded mb-2"><i
+                <a href="/expenses-category/1/edit" class="btn btn-sm btn-outline-primary rounded mb-2"><i
                         class="fas fa-plus"></i> Tambah</a>
-                <div class="float-right ml-2">
-                    <div class="input-group input-group-sm">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="basic-addon1"><i
-                                    class="fas fa-search"></i></span>
-                        </div>
-                        <input type="text" id="SearchBox" class="form-control form-control-sm dt-search"
-                            placeholder="Masukan Kata Kunci" aria-label="Username" aria-describedby="basic-addon1">
-                    </div>
-                </div>
-                <div class="float-right">
-                    <select name="lengthMenu" id="lengthMenu" class="form-control form-control-sm dt-length">
-                        <option value="10">10</option>
-                        <option value="20">20</option>
-                        <option value="50">50</option>
-                        <option value="100">100</option>
-                        <option value="-1">All</option>
-                    </select>
-                </div>
             </div>
             <hr>
             <div class="table-responsive p-3">
                 <table class="table align-items-center table-bordered table-striped table-hover w-100"
-                    id="dtDonorsCategory">
+                    id="table">
                     <thead class="thead-light">
                         <tr>
                             <th>No</th>
-                            <th>Gambar Buku</th>
-                            <th>Judul Buku</th>
-                            <th>Kategori</th>
-                            <th>Penulis</th>
-                            <th>Penerbit</th>
-                            <th>Tahun Terbit</th>
-                            <th class="px-5">Aksi</th>
+                            <th>Nama Kategori Pengeluaran</th>
+                            <th class="px-5 text-center">Aksi</th>
                         </tr>
                     </thead>
                     <tbody>
-                      <tr>
-                        <td>1</td>
-                        <td>1</td>
-                        <td>1</td>
-                        <td>1</td>
-                        <td>1</td>
-                        <td>1</td>
-                        <td>1</td>
-                        <td>1</td>
-                      </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>pembelian barang</td>
+                            <td class="px-5 text-center">
+                                <a href="/expenses-category/1/edit" class="btn btn-success"><i class="fas fa-pen"></i></a>
+                                <a href="" class="btn btn-danger"><i class="fas fa-trash"></i></a>
+                            </td>
+                        </tr>
                     </tbody>
                 </table>
             </div>
         </div>
-          <div class="card mb-4 dt-container">
-            <div class="col-lg-12 mt-3">
-                <div class="btn-group dropright">
-                    <button type="button" class="btn btn-sm btn-outline-secondary rounded mb-2"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="fas fa-download"></i>
-                    </button>
-                    <div class="dropdown-menu w-100 text-center">
-                        <button class="btn btn-sm btn-success col w-75 mb-2"><i class="fas fa-file-excel dt-excel"></i> Excel</button>
-                        <button class="btn btn-sm btn-danger col w-75 mb-2"><i class="fas fa-file-pdf dt-pdf"></i> PDF</button>
-                        <button class="btn btn-sm btn-secondary col w-75 mb-2"><i class="fas fa-print dt-print"></i> Print</button>
-                    </div>
-                </div>
-                <a href="/books/create" class="btn btn-sm btn-outline-primary rounded mb-2"><i
-                        class="fas fa-plus"></i> Tambah</a>
-                <div class="float-right ml-2">
-                    <div class="input-group input-group-sm">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="basic-addon1"><i
-                                    class="fas fa-search"></i></span>
-                        </div>
-                        <input type="text" id="SearchBox" class="form-control form-control-sm dt-search"
-                            placeholder="Masukan Kata Kunci" aria-label="Username" aria-describedby="basic-addon1">
-                    </div>
-                </div>
-                <div class="float-right">
-                    <select name="lengthMenu" id="lengthMenu" class="form-control form-control-sm dt-length">
-                        <option value="10">10</option>
-                        <option value="20">20</option>
-                        <option value="50">50</option>
-                        <option value="100">100</option>
-                        <option value="-1">All</option>
-                    </select>
-                </div>
-            </div>
-            <hr>
-            <div class="table-responsive p-3">
-                <table class="table align-items-center table-bordered table-striped table-hover w-100"
-                    id="dtDonorsCategory">
-                    <thead class="thead-light">
-                        <tr>
-                            <th>No</th>
-                            <th>Gambar Buku</th>
-                            <th>Judul Buku</th>
-                            <th>Kategori</th>
-                            <th>Penulis</th>
-                            <th>Penerbit</th>
-                            <th>Tahun Terbit</th>
-                            <th class="px-5">Aksi</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>1</td>
-                        <td>1</td>
-                        <td>1</td>
-                        <td>1</td>
-                        <td>1</td>
-                        <td>1</td>
-                        <td>1</td>
-                        <td>1</td>
-                      </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-
         </section>
       </div>
 @endsection
